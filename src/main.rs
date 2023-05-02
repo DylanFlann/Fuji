9dcad52daa8c764c878ec52e1e60686d9f000180
Thomas Marchand <thomas.marchand@tuta.io>
2023-05-02 11:04:25 +0200

    feat: allow the analysor to restart from a cursor when connection is reset

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate lazy_static;
 use apibara_core::starknet::v1alpha2::{Block, Filter};
 use apibara_sdk::ClientBuilder;
+use processing::ProcessingError;
 mod apibara;
 mod config;
 mod listeners;
@@ -10,15 +11,26 @@ mod processing;
 #[tokio::main]
 async fn main() {
     let conf = config::load();
-    let apibara_conf = apibara::create_apibara_config(&conf);
-    let uri = conf.apibara.stream.parse().unwrap();
-    let (mut data_stream, data_client) = ClientBuilder::<Filter, Block>::default()
-        .connect(uri)
-        .await
-        .unwrap();
+    let mut cursor_opt = None;
+    loop {
+        let apibara_conf = apibara::create_apibara_config(&conf, cursor_opt.clone());
+        let uri = conf.apibara.stream.parse().unwrap();
+        let (mut data_stream, data_client) = ClientBuilder::<Filter, Block>::default()
+            .connect(uri)
+            .await
+            .unwrap();
 
-    data_client.send(apibara_conf).await.unwrap();
-    processing::process_data_stream(&mut data_stream, &conf)
-        .await
-        .unwrap();
+        data_client.send(apibara_conf).await.unwrap();
+        match processing::process_data_stream(&mut data_stream, &conf).await {
+            Err(e) => {
+                if let Some(ProcessingError::CursorError(cursor_opt2)) =
+                    e.downcast_ref::<ProcessingError>()
+                {
+                    cursor_opt = cursor_opt2.clone();
+                }
+                println!("connection reset, restarting from last cursor");
+            }
+            _ => {}
+        }
+    }
 }
